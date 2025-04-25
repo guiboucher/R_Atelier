5dfe8db4eee5a147587a6b6e8e69135f5af29c38
Guillaume Boucher <guiboucher8@gmail.com>
2025-04-25 09:33:36 -0400

    -

---
+++ RStudio et R de base.R
@@ -0,0 +1,30 @@
+
+# Introduction -----------------------------------------------------------------------------
+
+## Ouverture de RStudio ####
+
+## Quatre fenêtres ####
+
+### Script R - Haut gauche ####
+# Cliquer sur Run en haut à droite du script ou CTRL+ENTER
+5 + 5
+# Une ligne de commentaire commence par le caractère dièse [#]
+# 5 + 5 ne sera pas exécuté puisque la ligne de code commence par un dièse
+
+### Console - Bas gauche ####
+# On y voit le résultat de notre calcul précédent qui était 5+5
+
+### Environnement et autres - Haut droit ####
+
+#### Environnement ####
+# Endroit où l'on peut voir la ou les valeurs d'un objet ou d'une variable.
+
+x <- 5 + 5
+#### History ####
+# Historique du code exécuté
+
+#### Connections ####
+# Connexion sur une base de données. Dans notre cas ce sera à Teradata.
+
+#### Tutorial ####
+# Différentes formations de R.
